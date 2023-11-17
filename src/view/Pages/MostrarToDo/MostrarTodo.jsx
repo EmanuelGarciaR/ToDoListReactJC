c68cfb12324155ded55469409db3c504ef39f1cb
EmanuelGarcia <emanuelgarciarios@gmail.com>
2023-11-16 23:00:34 -0500

    18. Estilos para las Tareas

--- src/view/Pages/MostrarToDo/MostrarTodo.jsx
+++ src/view/Pages/MostrarToDo/MostrarTodo.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react";
 import { Get_TODOS } from "../../../services/fetchApi";
 import { TodoContext } from "../../context/todoContext";
 import { TaskContext } from "../../context/user";
-// import "./Form.css";
+import "./MostrarTodo.css";
 
 export const GetToDos = () => {
     const { init, dispatch, getTodo } = useContext(TodoContext);
@@ -25,9 +25,12 @@ export const GetToDos = () => {
     return (
         task && Array.isArray(task) ? (
             task.map((todo) => (
-                <div key={todo._id} className='todo_container'>
-                    <h3>{todo.name}</h3>
-                    <div>{todo.description}</div>
+                <div key={todo._id} className='container-todo'>
+                    <div className="todo__container">
+                        <h3 className="todo__title">{todo.name}</h3>
+                        <div className="todo__description">{todo.description}</div>
+                        <div className="todo__date">{todo.finishDate}</div>
+                    </div>
                 </div>
             ))
         ) : (
